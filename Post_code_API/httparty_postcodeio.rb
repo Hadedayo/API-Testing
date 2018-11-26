a2e6c0281b4b70c15e360d102cfc1a0398d05872
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 16:38:17 +0000

    Included the module (HTTparty) and the base_uri. Created method (get_single_postcode ) and included attribute accessor

--- Post_code_API/httparty_postcodeio.rb
+++ Post_code_API/httparty_postcodeio.rb
@@ -2,6 +2,17 @@ require 'httparty'
 require 'json'
 
 class Postcodeio
+  include HTTParty
 
+  attr_accessor :single_postcode_results, :multiple_postcode_results
 
+  base_uri 'https://api.postcodes.io'
+
+  def get_single_postcode (postcode)
+    @single_postcode_results = self.class.get("/postcodes/#{postcode}")
+    puts JSON.parse(@single_postcode_results.body)
+  end
 end
+
+single = Postcodeio.new
+single.get_single_postcode("TW106NF")

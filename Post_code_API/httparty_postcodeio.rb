2679c2373a447cc0c83161ff665dc1de5c69ecce
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 17:21:22 +0000

    Creating a method for posting multiple postcodes

--- Post_code_API/httparty_postcodeio.rb
+++ Post_code_API/httparty_postcodeio.rb
@@ -12,7 +12,17 @@ class Postcodeio
     @single_postcode_results = self.class.get("/postcodes/#{postcode}")
     puts JSON.parse(@single_postcode_results.body)
   end
+
+  def post_multiple_postcode (postcodes)
+    @multiple_postcode_results = self.class.post("/postcodes/", body:{"postcodes": postcodes})
+    puts JSON.parse(@multiple_postcode_results.body)
+  end
+
+
+
 end
 
 single = Postcodeio.new
 single.get_single_postcode("TW106NF")
+
+single.post_multiple_postcode(["IG110RU", "TW106NF"])

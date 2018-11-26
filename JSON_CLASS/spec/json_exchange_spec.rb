7e50f3d2f1c667e2348779ab578b99b57c36b11a
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-26 15:28:21 +0000

    Added api key configuration and replaced @exchange_rates to @file, where @file gets access to the online api.

--- JSON_CLASS/spec/json_exchange_spec.rb
+++ JSON_CLASS/spec/json_exchange_spec.rb
@@ -1,53 +1,48 @@
 describe "Testing the exchange rates" do
 
   before(:all) do
-    @exchange_rates = ParseJson.new(HTTParty::get("http://data.fixer.io/api/latest?access_key=#{@api_key}").body))
+    @api_key = ENV['FIXER_API_KEY']
+    @file = ParseJson.new(HTTParty::get("http://data.fixer.io/api/latest?access_key=#{@api_key}").body)
   end
 
   it "should be a Hash" do
-    expect(@exchange_rates.json_file).to be_kind_of (Hash)
+    expect(@file.json_file).to be_kind_of (Hash)
   end
 
   it "should have base as EUR " do
-    expect(@exchange_rates.get_base).to eq 'EUR'
+    expect(@file.get_base).to eq 'EUR'
   end
 
   it "should respond to the correct date " do
-    expect(@exchange_rates.get_date).to eq "2017-07-26"
+    expect(@file.get_date).to eq "2018-11-26"
   end
 
   it "should have individual keys in the json_exchange as strings" do
-    @exchange_rates.json_file.each do |key, value|
+    @file.json_file.each do |key, value|
       expect(key).to be_kind_of(String)
     end
   end
 
   it "should have individual values in the rates as a Numeric" do
-    @exchange_rates.json_file['rates'].values.each do |value|
+    @file.json_file['rates'].values.each do |value|
       expect(value).to be_kind_of(Numeric)
     end
   end
 
-  it "should have individual values in the rates as a Float" do
-    @exchange_rates.json_file['rates'].values.each do |value|
-      expect(value).to be_kind_of(Float)
-    end
-  end
-
   it "should have the number of characters to be 3 in each key for rates" do
-    @exchange_rates.json_file['rates'].keys.each do |key|
+    @file.json_file['rates'].keys.each do |key|
       expect(key.length).to eq 3
     end
   end
 
   it "should return true if the values of rates are > zero" do
-    @exchange_rates.json_file['rates'].values.each do |value|
+    @file.json_file['rates'].values.each do |value|
       expect(value).to be > 0
     end
   end
 
   it "should return false if the values of rates are not > zero" do
-    @exchange_rates.json_file['rates'].values.each do |value|
+    @file.json_file['rates'].values.each do |value|
       expect(value).to be > 0
     end
   end
